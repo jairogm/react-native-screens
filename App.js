4a77ba3f1021d3d3c85d4c4fd47cbd06cc78a445
503 <503@academicos.cesde.edu.co>
2023-04-20 21:04:51 -0500

    tercer versionaminto...

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Contacts from './screens/Contacts';
-import Loguin from './screens/loguin';
+import Loguin from './screens/Loguin';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons'
+import { TextInput, Button } from 'react-native-paper';
 
 
 //crear constante para generar las rutas de las pantallas(screens)
+
+let users = [
+  {username: 'hgj',name:'anderson',password:22,role:1},
+  {username: 'gfhd',name:'anderson',password:22,role:1}
+]
+
+
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Products" component={ProductsScreen} options={{title:'Productos'}}/>
-        <Stack.Screen name="Home" component={HomeScreen} options={{title:'Inicio'}}/>
+        <Stack.Screen name="HomeTabs" component={HomeTabs} options={{title:'Sistema tecnologico'}}/>
+        {/* <Stack.Screen name="Home" component={HomeScreen} options={{title:'Inicio'}}/>
         <Stack.Screen name="Contacts" component={Contacts} options=      {{title:'Contactanos'}}/>
-        <Stack.Screen name='Loguin'component={Loguin} options={{title:'Loguin'}}/>
+        <Stack.Screen name='Loguin'component={Loguin} options={{title:'Loguin'}}/> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -22,19 +34,19 @@ export default function App() {
 function HomeScreen({navigation}){
   return(
     <View style={styles.container}>
-      <Text style={{marginBottom:20}}>Estamos en Inicio</Text>
-      <Button
+      <Text style={{marginBottom:20}}>Inicio de Sección</Text>
+      {/* <Button
       title=" Ir a Productos"
       onPress={() =>{
       navigation.navigate('Products')
       }}
-      />
-      <Button
+      /> */}
+      {/* <Button
       title="Registrate"
       onPress={() =>{
       navigation.navigate('Loguin')
       }}
-      />
+      /> */}
 
     </View>
   );
@@ -45,22 +57,39 @@ function ProductsScreen({navigation}){
   return(
     <View style={styles.container}>
       <Text style={{marginBottom:20}}>Estamos en Productos</Text>
-      <Button
-      title="Contact"
-      onPress={()=>{
-      navigation.navigate('Contacts',{title:title,name:fullname})
-      }}
-
-      />
-
     </View>
+  );
+}
+
+function HomeTabs(){
+  return(
+    <Tab.Navigator
+      screenOptions={{
+        headerShown:false, //lo que hace es no mostrar cada tittulo del componente....
+        tabBarActiveTintColor:'Blue',
+        tabBarInactiveTintColor:'red',
+        tabBarActiveBackgroundColor:'powderblue',
+        tabBarInactiveBackgroundColor:'blue'
+      }}
+    >
+      <Tab.Screen name='Home' component={HomeScreen} options={{
+        tabBarStyle:{display:'none'},
+        tabBarIcon:(tabInfo) => (<MaterialIcons name='home' size={22}/>)
+      }}/>
+      <Tab.Screen name='Products' component={ProductsScreen} options={{
+        tabBarIcon:(tabInfo) => (<MaterialIcons name='category' size={22}/>)
+      }}/>
+      <Tab.Screen name='Contacts' component={Contacts} options={{
+        tabBarIcon:(tabInfo) => (<MaterialIcons name='call' size={22}/>)
+      }}/>
+    </Tab.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
